0f5caacbcae6211d6ea330e655986540b6b8ae53
hypothete <erin@hypothete.com>
2026-07-05 08:45:09 -0700

    added xy controls and a zoom toggle

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 import * as THREE from "three/webgpu";
-import { instancedArray, instanceIndex, texture, uniform, uv } from "three/tsl";
+import { distance, float, instancedArray, instanceIndex, step, texture, uniform, uv, vec2 } from "three/tsl";
 import "./style.css";
 import { buildDots } from "./dots";
 
 const { dots, flatDots } = buildDots();
 const dotsArray = instancedArray(flatDots, "vec2");
 
-const apertureSlider: HTMLInputElement | null =
-  document.querySelector("#aperture");
+const zoominCheckbox: HTMLInputElement | null = document.querySelector("#zoomin");
+const apertureSlider: HTMLInputElement | null = document.querySelector("#aperture");
 const focusSlider: HTMLInputElement | null = document.querySelector("#focus");
-
-const closeUp = false ? 0.1 : 1;
+const xOffsetSlider: HTMLInputElement | null = document.querySelector("#xoffset");
+const yOffsetSlider: HTMLInputElement | null = document.querySelector("#yoffset");
 
 const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera(0, closeUp, closeUp, 0, 1, -1);
+const camera = new THREE.OrthographicCamera(0, 1, 1, 0, 1, -1);
 
-const renderer = new THREE.WebGPURenderer({ antialias: false });
+const renderer = new THREE.WebGPURenderer({ antialias: true });
 renderer.setSize(820, 820);
 renderer.setClearColor(0x000000, 1);
 renderer.setAnimationLoop(render);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(0.001, 0.001);
+const geometry = new THREE.PlaneGeometry(0.01, 0.01);
 const material = new THREE.MeshBasicNodeMaterial({
   // color: 0x300000,
   blending: THREE.AdditiveBlending,
@@ -32,13 +32,15 @@ const textureLoader = new THREE.TextureLoader();
 const imgTex = textureLoader.load("public/test.png");
 
 const focusUniform = uniform(Number(focusSlider?.value || 1));
+const apertureUniform = uniform(Number(apertureSlider?.value || 1));
+const offsetUniform = uniform(new THREE.Vector2(Number(xOffsetSlider?.value || 0), Number(yOffsetSlider?.value || 0)));
 
 const texNode = texture(
   imgTex,
-  dotsArray.element(instanceIndex).sub(uv().sub(0.5).div(1000).mul(focusUniform)),
+  dotsArray.element(instanceIndex).add(offsetUniform.div(1000)).sub(uv().sub(0.5).div(1000).mul(focusUniform)),
 );
 
-material.colorNode = texNode;
+material.colorNode = texNode.mul(step(apertureUniform.div(200), float(0.5).sub(distance(vec2(0.5),uv()))));
 
 const instances = new THREE.InstancedMesh(geometry, material, dots.length);
 
@@ -53,7 +55,9 @@ instances.instanceMatrix.needsUpdate = true;
 scene.add(instances);
 
 apertureSlider?.addEventListener("input", () => {
-  adjustApertures(0.01 * Number(apertureSlider.value));
+  apertureUniform.value = Number(apertureSlider?.value || 0);
+  apertureUniform.needsUpdate = true;
+  renderer.render(scene, camera);
 });
 
 focusSlider?.addEventListener("input", () => {
@@ -62,17 +66,25 @@ focusSlider?.addEventListener("input", () => {
   renderer.render(scene, camera);
 });
 
-function render() {
+xOffsetSlider?.addEventListener("input", () => {
+  offsetUniform.value.x = Number(xOffsetSlider?.value || 0);
+  focusUniform.needsUpdate = true;
   renderer.render(scene, camera);
-}
+});
 
-function adjustApertures(size: number) {
-  dots.forEach((dot, dotIndex) => {
-    const dotMatrix = new THREE.Matrix4();
-    dotMatrix.makeScale(size, size, size);
-    dotMatrix.setPosition(new THREE.Vector3(dot.x, dot.y, 0));
-    instances.setMatrixAt(dotIndex, dotMatrix);
-  });
-  instances.instanceMatrix.needsUpdate = true;
+yOffsetSlider?.addEventListener("input", () => {
+  offsetUniform.value.y = Number(yOffsetSlider?.value || 0);
+  focusUniform.needsUpdate = true;
+  renderer.render(scene, camera);
+});
+
+zoominCheckbox?.addEventListener("input", () => {
+  camera.right = zoominCheckbox.checked ? 0.1 : 1;
+  camera.top = zoominCheckbox.checked ? 0.1 : 1;
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
+})
+
+function render() {
   renderer.render(scene, camera);
 }
