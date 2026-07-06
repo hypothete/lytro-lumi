d45a8906aca3c441160389070b6404739c319707
hypothete <erin@hypothete.com>
2026-07-06 10:25:25 -0700

    aah relative path

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ const rgbMaterial = new THREE.MeshBasicNodeMaterial({
 });
 
 const textureLoader = new THREE.TextureLoader();
-const imgTex = textureLoader.load("/test.jpg");
+const imgTex = textureLoader.load("./test.jpg");
 imgTex.colorSpace = THREE.SRGBColorSpace;
 
 const focusUniform = uniform(Number(focusSlider?.value || 1));
